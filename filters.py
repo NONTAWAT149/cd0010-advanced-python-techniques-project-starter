6f664eab8e2569ea2cec2d60bfd86bdbf297a69a
NONTAWAT149 <nontawat149@gmail.com>
2023-01-29 23:23:50 +0000

    update filter

--- filters.py
+++ filters.py
@@ -71,6 +71,32 @@ class AttributeFilter:
     def __repr__(self):
         return f"{self.__class__.__name__}(op=operator.{self.op.__name__}, value={self.value})"
 
+class DateFilter(AttributeFilter):
+    @classmethod
+    def get(cls, approach):
+        return approach.time.date()
+
+
+class DistanceFilter(AttributeFilter):
+    @classmethod
+    def get(cls, approach):
+        return approach.distance
+
+class VelocityFilter(AttributeFilter):
+    @classmethod
+    def get(cls, approach):
+        return approach.velocity
+
+class DiameterFilter(AttributeFilter):
+    @classmethod
+    def get(cls, approach):
+        return approach.neo.diameter
+
+class Hazardous(AttributeFilter):
+    @classmethod
+    def get(cls, approach):
+        return approach.neo.hazardous
+
 
 def create_filters(
         date=None, start_date=None, end_date=None,
@@ -109,7 +135,39 @@ def create_filters(
     :return: A collection of filters for use with `query`.
     """
     # TODO: Decide how you will represent your filters.
-    return ()
+    filter_list = []
+
+    if date:
+        filter_list.append(DateFilter(operator.eq, date))
+
+    if start_date:
+        filter_list.append(DateFilter(operator.ge, start_date))
+
+    if end_date:
+        filter_list.append(DateFilter(operator.le, end_date))
+
+    if distance_min:
+        filter_list.append(DistanceFilter(operator.ge, distance_min))
+
+    if distance_max:
+        filter_list.append(DistanceFilter(operator.le, distance_max))
+
+    if velocity_min:
+        filter_list.append(VelocityFilter(operator.ge, velocity_min))
+
+    if velocity_max:
+        filter_list.append(VelocityFilter(operator.le, velocity_max))
+
+    if diameter_min:
+        filter_list.append(DiameterFilter(operator.ge, diameter_min))
+
+    if diameter_max:
+        filter_list.append(DistanceFilter(operator.le, distance_max))
+
+    if hazardous in (True, False):
+        filter_list.append(Hazardous(operator.eq, hazardous))
+
+    return filter_list
 
 
 def limit(iterator, n=None):
@@ -122,4 +180,5 @@ def limit(iterator, n=None):
     :yield: The first (at most) `n` values from the iterator.
     """
     # TODO: Produce at most `n` values from the given iterator.
+
     return iterator
